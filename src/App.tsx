6d1b6a53fc60cf71350a9fd055c75843e774efc5
alalUDDIN123 <alalu75666@gmail.com>
2024-06-26 15:54:21 +0530

    ---components--in--react--26-06-2024 03:54 PM---

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
 
- const App = () => {
+// Define the props type
+type GreetingProps = {
+  name: string;
+};
+
+// Styled components
+const GreetingWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const GreetingText = styled.h1`
+  color: #4a90e2;
+  font-family: 'Arial', sans-serif;
+  font-size: 2.5rem;
+`;
+
+// Greeting component
+const Greeting: React.FC<GreetingProps> = (props) => {
   return (
-    <div>Hello World</div>
-  )
-}
+    <GreetingWrapper>
+      <GreetingText>Hello, {props.name}!</GreetingText>
+    </GreetingWrapper>
+  );
+};
 
+// App component
+const AppWrapper = styled.div`
+  font-family: 'Arial', sans-serif;
+  text-align: center;
+`;
 
-export default App
+const App = () => {
+  return (
+    <AppWrapper>
+      <Greeting name="Alal Uddin" />
+    </AppWrapper>
+  );
+};
+
+export default App;
