db7a9b1099f03332ef4d0502e2beab010722ed48
alalUDDIN123 <alalu75666@gmail.com>
2024-06-27 10:01:18 +0530

    --toggle--switch---27-06-2024--

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-
+import { useState } from "react";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <ToggleSwitch />
+    </>
+  );
+};
 
-export default App
+const ToggleSwitch = () => {
+  const [isOn, setIsOn] = useState(false)
+
+  const handleToggle = () => {
+    setIsOn(!isOn)
+  }
+
+  const isToogle = isOn ? 'on' : 'off';
+  return (
+    <>
+      <h2 className="switch-state">Toggle Switch</h2>
+      <div className="toggle-switch" onClick={handleToggle} >
+        <div className={`switch ${isToogle}`}></div>
+      </div>
+    </>
+  );
+};
+
+export default App;
