5be157efbb617c04afe0e68f87b57f3e3cfccf34
alalUDDIN123 <alalu75666@gmail.com>
2024-06-27 19:06:09 +0530

    ---update--code---for--theme--27-06-2024 07:06 PM---

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThemeProvider } from './ThemeContext.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </>,
 )
